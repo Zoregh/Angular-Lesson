55101b0e301baa93433ffbd85744a41407b6a7d5
Zoregh <zoreghhakobyan@gmail.com>
2019-01-23 10:26:54 +0400

    remoove first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './Components/first/first.component';
 import { FruitsComponent } from './Components/fruits/fruits.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
     FruitsComponent,
     NavbarComponent
   ],
