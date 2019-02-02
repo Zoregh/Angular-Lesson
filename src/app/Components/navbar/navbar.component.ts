b4d0988948fbc3413e7ffc8ff538022025c98ef1
Zoregh <zoreghhakobyan@gmail.com>
2019-02-02 12:07:17 +0400

    cart moved to the navbar

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneralService } from '../../Services/general.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pruductService:GeneralService) { }
 
   ngOnInit() {
   }
