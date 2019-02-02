1d1d608702f6196a6df3b33b3e13bdb8d176cb84
Zoregh <zoreghhakobyan@gmail.com>
2019-02-02 11:32:15 +0400

    removed old components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,8 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FruitComponent } from './Components/fruit/fruit.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FirstComponent } from './Components/first/first.component';
-import { SecondComponent } from './Components/second/second.component';
-import { FruitsComponent } from './Components/fruits/fruits.component';
-import { VegetablesComponent } from './Components/vegetables/vegetables.component';
-import { VegetableComponent } from './Components/vegetable/vegetable.component';
 import { ProductComponent } from './Components/product/product.component';
 import { ProductsComponent } from './Components/products/products.component';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
@@ -22,13 +17,8 @@ import { Page404Component } from './Components/page404/page404.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FruitComponent,
     NavbarComponent,
     FirstComponent,
-    SecondComponent,
-    FruitsComponent,
-    VegetablesComponent,
-    VegetableComponent,
     ProductComponent,
     ProductsComponent,
     ProductDetailsComponent,
